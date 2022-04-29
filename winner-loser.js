2ff46285a760403478174cb1ff999c5ed8c21744
LierMutube <98529445+set09117@users.noreply.github.com>
2022-04-29 11:50:46 +0100

    update winner-loser + winner-loser css and js

---
+++ winner-loser.js
@@ -0,0 +1,7 @@
+
+var team1Score = localStorage.getItem("team1Score");
+var team2Score = localStorage.getItem("team2Score");
+
+document.getElementById("first-num").innerHTML = team1Score;
+
+document.getElementById("second-num").innerHTML = team2Score;
